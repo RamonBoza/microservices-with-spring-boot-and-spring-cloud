50fbe9d0d2a5e5078a660ffa090abf90f87c2ad1
Ramon Boza <bozanayade@gmail.com>
2018-12-31 12:59:25 +0100

    Adding generic validation and messages

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomzedResponseEntityExceptionHandler.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomzedResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
 import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,16 @@ public class CustomzedResponseEntityExceptionHandler extends ResponseEntityExcep
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 
         ResponseEntity responseEntity = new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+
+        return responseEntity;
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Failed", ex.getBindingResult().toString());
+
+        ResponseEntity responseEntity = new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
 
         return responseEntity;
     }
