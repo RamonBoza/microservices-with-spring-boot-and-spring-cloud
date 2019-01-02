3bbb166901542e67616f97dd5af93c1eaa9a447f
Ramon Boza <bozanayade@gmail.com>
2019-01-02 16:10:41 +0100

    Adding name and server to config and created Rest Controller for limits

---
+++ microservices/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,14 @@
+package com.in28minutes.microservices.limitsservice;
+
+import com.in28minutes.microservices.limitsservice.bean.LimitsConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @GetMapping("/limits")
+    public LimitsConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitsConfiguration(1000,1);
+    }
+}
